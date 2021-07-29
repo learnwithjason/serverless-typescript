963dafcf1a7d9bfdbd3bea366a8ce084f985814d
Jason Lengstorf <jason@lengstorf.com>
2021-07-29 10:21:48 -0700

    feat: add types

--- netlify/functions/hello-typescript/hello-typescript.ts
+++ netlify/functions/hello-typescript/hello-typescript.ts
@@ -1,13 +1,11 @@
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-export const handler = async (event) => {
+import { HandlerEvent } from '@netlify/functions';
+
+export const handler = async (event: HandlerEvent) => {
   try {
-    const subject = event.queryStringParameters.name || 'Wow';
+    const subject = event?.queryStringParameters?.name || 'Wow';
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
     };
   } catch (error) {
     return { statusCode: 500, body: error.toString() };
